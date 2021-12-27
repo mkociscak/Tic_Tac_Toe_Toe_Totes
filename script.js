933b906cb50b93683cea7b51d43a37c0fc0c6f4c
Megan Kociscak <megankociscak@gmail.com>
2021-12-26 21:44:49 -0600

    all files in progress - added wireframe 02

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const gameTitle = document.querySelector(".gameTitle")  //gameTitle
 const playerX = document.querySelector(".playerX")      //playerX
 const playerO = document.querySelector(".playerO")      //playerO
+const resetButton = document.querySelector(".resetButton")    //reset
 const gridBoard = document.querySelector(".gridBoard")  //gridBoard
 
+const allSquares = Array.from(document.getElementsByClassName('square'))
+console.log(allSquares)
+let gridSquares = ['', '', '', '', '', '', '', '', '']
+let playerUp = ''
+let gameActive = true
+
+resetButton.addEventListener('click', resetBoard)
+
+/*--
 whoseTurn()
 function whoseTurn() {
     let playerX = document.getElementByClassName("playerX")
@@ -12,4 +22,13 @@ function whoseTurn() {
     } else {
         playerOGo()
     }
+}
+----------------------GAME PLAY-----------------------------*/
+
+
+
+
+/*-----------------------------------------------------*/
+function resetBoard() {
+    let 
 }
